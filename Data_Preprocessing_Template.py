8e1a23ec61953da4b09037c0161099f9837c18e2
Nikhil Arroju <nikhilarroju47@gmail.com>
2018-10-29 13:42:33 +0530

    Adding the data preprocessing template

---
+++ Data_Preprocessing_Template.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 30 18:45:50 2018
+
+@author: Nik
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#This is my datapreprocessing template
+dataset = pd.read_csv('Data.csv')
+
+X = dataset.iloc[:,:3].values
+y = dataset.iloc[:,-1].values
+
+from sklearn.preprocessing import Imputer
+#This is used to deal with the missing values
+imputer = Imputer(missing_values="NaN",strategy="mean",axis=0)
+X[:,1:3] = imputer.fit_transform(X[:,1:3])
+#X[:,1:3] = imputer.transform(X[:,1:3])
+
+from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,0] = labelencoder_X.fit_transform(X[:,0])
+onehotencoder = OneHotEncoder(categorical_features=[0])
+X = onehotencoder.fit_transform(X).toarray()
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.2,random_state = 42)
+
+#feature scaling
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.fit_transform(X_test)
+
+
+
+
